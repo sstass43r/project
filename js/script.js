becefb480e68aeb727ed4daa582bc933e1293030
St <sstass43r@mail.ru>
2023-09-17 21:26:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"user strict";
+
+// alert("Hello");
+
+// const result = confirm("ТЫ тут?");
+// console.log(result);
+
+// const anser = prompt("Тебе 18?", "18");
+// console.log(typeof anser);
+
+const answer = [];
+answer[0] = prompt("как ваше имя?", "");
+answer[1] = prompt("как ваша фамилия?", "");
+answer[3] = prompt("лет?", "");
+
+document.write(answer);
